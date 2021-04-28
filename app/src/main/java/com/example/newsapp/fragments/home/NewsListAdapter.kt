980bf13843b8b9261cf7ae1289c64a7be18f706f
ycannot <aslanycan@gmail.com>
2021-04-29 01:08:57 +0300

    newsListAdapter new function named "formatDateString"

--- app/src/main/java/com/example/newsapp/fragments/home/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsapp/fragments/home/NewsListAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.newsapp.fragments.home
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.databinding.ListItemNewsBinding
 import com.example.newsapp.models.Article
 import com.squareup.picasso.Picasso
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsListAdapter(
     var articles: ArrayList<Article> = arrayListOf(),
-    var listener: ((url:String) -> Unit)? = null
+    var listener: ((url: String) -> Unit)? = null
 ) : RecyclerView.Adapter<NewsListAdapter.ViewHolder>() {
     private lateinit var binding: ListItemNewsBinding
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -36,11 +38,23 @@ class NewsListAdapter(
             itemBinding.tvTitle.text = article.title
             itemBinding.tvDescription.text = article.description
             itemBinding.tvSource.text = article.source?.name
-            itemBinding.tvDate.text = article.publishedAt
+            itemBinding.tvDate.text = formatDateString(article.publishedAt)
             itemBinding.root.setOnClickListener {
                 article.url?.let {
                     listener?.invoke(it)
                 }
+            }
+        }
+        private fun formatDateString(dateString: String?):String{
+            if (dateString == null){
+                return "Date"
+            }
+            val fmt = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+            return try {
+                val date: Date = fmt.parse(dateString) ?: Date()
+                fmt.format(date)
+            } catch (pe: ParseException) {
+                "Date"
             }
         }
     }
