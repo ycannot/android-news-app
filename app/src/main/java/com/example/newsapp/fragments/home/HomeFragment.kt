aaa0fc9129c7995d424b0a82a37432c42f870162
ycannot <aslanycan@gmail.com>
2021-04-28 22:45:53 +0300

    HomeFragment optimizations

--- app/src/main/java/com/example/newsapp/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/home/HomeFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.newsapp.MainActivity
-import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentHomeBinding
 import com.example.newsapp.fragments.base.BaseFragment
 import com.example.newsapp.helpers.AppbarHelper
@@ -19,6 +17,7 @@ class HomeFragment : BaseFragment() {
     lateinit var viewModel: HomeViewModel
     lateinit var appbarHelper: AppbarHelper
     lateinit var startDateString: String
+    private val newsListAdapter = NewsListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,29 +29,27 @@ class HomeFragment : BaseFragment() {
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         appbarHelper = (requireActivity() as MainActivity).appbarHelper
         appbarHelper.setTitle("Latest News")
+        binding.rvNews.adapter = newsListAdapter
         initObservers()
         return binding.root
     }
 
     private fun initObservers() {
-        (requireActivity() as MainActivity).dateLiveData.observe(viewLifecycleOwner, Observer {
-            viewModel.getFootballNews(it)
-            startDateString = it
+        (requireActivity() as MainActivity).dateLiveData.observe(viewLifecycleOwner, Observer { dateString ->
+            viewModel.getFootballNews(dateString)
+            startDateString = dateString
         })
         viewModel.newsLiveData.observe(viewLifecycleOwner, Observer { result ->
             result?.let {
                 result.articles?.let {
-                    val adapter = NewsListAdapter(arrayListOf())
-                    binding.rvNews.adapter = adapter
-                    adapter.articles.clear()
-                    adapter.articles.addAll(it)
-                    adapter.notifyDataSetChanged()
-
+                    newsListAdapter.articles.clear()
+                    newsListAdapter.articles.addAll(it)
+                    newsListAdapter.notifyDataSetChanged()
                 }
             }
         })
-        viewModel.apiFailLiveData.observe(viewLifecycleOwner, Observer {
-            println(it)
+        viewModel.apiFailLiveData.observe(viewLifecycleOwner, Observer { apiFail ->
+            Toast.makeText(requireContext(), "${apiFail.code}: ${apiFail.message}" , Toast.LENGTH_SHORT).show()
         })
     }
 
